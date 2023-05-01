1122aa99e2a40529337083ab6e285d57e25cb5ed
RajuUnjiya <raju.unjiya@acuteinformatics.in>
2023-05-01 15:47:50 +0530

    Remove files

--- src/registry/fns/functions/general.ts
+++ src/registry/fns/functions/general.ts
@@ -22,33 +22,6 @@ const GeneralAPISDK = () => {
       throw DefaultErrorObject(message, messageDetails);
     }
   };
-  const GetChargeTemplates = async () => {
-    const { data, status, message, messageDetails } =
-      await AuthSDK.internalFetcher(
-        "GETCHARGETEMPLATELIST",
-        {},
-        {
-          UNIQUE_REQ_ID: "32627636893400",
-          APITOKEN: "MzI2Mjc2MzY4OTM0MDA=",
-        }
-      );
-    if (status === "0") {
-      let responseData = data;
-      if (Array.isArray(responseData)) {
-        responseData = responseData.map(({ VALUE, LABEL, ...other }) => {
-          //let { VALUE, LABEL, ...other } = one;
-          return {
-            value: VALUE,
-            label: LABEL,
-            ...other,
-          };
-        });
-      }
-      return responseData;
-    } else {
-      throw DefaultErrorObject(message, messageDetails);
-    }
-  };
   const getValidateValue = async (fieldData) => {
     if (
       //fieldData.value === "X" || --if any issue doing validation uncomment and check
@@ -64,9 +37,7 @@ const GeneralAPISDK = () => {
       return "";
     }
   };
-  const putOpenWindowName = async (item) => {
-    //console.log(item);
-  };
+  
   const getTranslateDataFromGoole = async (data, fromLang, toLang) => {
     try {
       let response = await fetch(
@@ -92,71 +63,17 @@ const GeneralAPISDK = () => {
       return "";
     }
   };
-  const GetDistrictList = async () => {
-    const { data, status, message, messageDetails } =
-      await AuthSDK.internalFetcher(
-        "GETDISTRICTLIST",
-        {},
-        {
-          UNIQUE_REQ_ID: "32627636893400",
-          APITOKEN: "MzI2Mjc2MzY4OTM0MDA=",
-        }
-      );
-    if (status === "0") {
-      let responseData = data;
-      if (Array.isArray(responseData)) {
-        responseData = responseData.map(({ NEW_DIST_CD, DIST_NM }) => {
-          return {
-            value: NEW_DIST_CD,
-            label: DIST_NM,
-          };
-        });
-      }
-      return responseData;
-    } else {
-      throw DefaultErrorObject(message, messageDetails);
-    }
-  };
+  
   const setDocumentName = (text) => {
     let titleText = document.title;
     document.title = titleText.split(" - ")[0] + " - " + text;
   };
-  const GetSecurityGroupingList = async () => {
-    const { data, status, message, messageDetails } =
-      await AuthSDK.internalFetcher("GETSECURITYGROUPINGLIST", {});
-    if (status === "0") {
-      return data.map((item) => {
-        return { label: item?.GROUP_NAME, value: item?.GROUP_NAME };
-      });
-    } else {
-      throw DefaultErrorObject(message, messageDetails);
-    }
-  };
-  const GetUsersNotificationTemplateList = async () => {
-    const { data, status, message, messageDetails } =
-      await AuthSDK.internalFetcher("GETUSERSNOTIFTEMPLIST", {});
-    if (status === "0") {
-      return data.map((item) => {
-        return {
-          label: item?.DESCRIPTION,
-          value: item?.TRAN_CD,
-          disabled: item?.CONFIRMED === "N" ? true : false,
-        };
-      });
-    } else {
-      throw DefaultErrorObject(message, messageDetails);
-    }
-  };
+  
   return {
     GetMiscValue,
     getValidateValue,
-    putOpenWindowName,
-    GetChargeTemplates,
     getTranslateDataFromGoole,
-    GetDistrictList,
     setDocumentName,
-    GetSecurityGroupingList,
-    GetUsersNotificationTemplateList,
   };
 };
 
--- src/registry/fns/registerFnsNetBanking.ts
+++ src/registry/fns/registerFnsNetBanking.ts
@@ -1,9 +1,6 @@
 import { singletonFunctionRegisrationFactory } from "components/utils";
-import { GetEmailAcctConfigDD } from "pages_audit/pages/configurations/emailSMSTemplate/emailSMSTemplateForm/api";
 import { GeneralAPI } from "./functions";
 const { registerFn } = singletonFunctionRegisrationFactory;
 
 registerFn("GetMiscValue", GeneralAPI.GetMiscValue);
 registerFn("getValidateValue", GeneralAPI.getValidateValue);
-registerFn("GetChargeTemplates", GeneralAPI.GetChargeTemplates);
-registerFn("GetEmailAcctConfigDD", GetEmailAcctConfigDD);
