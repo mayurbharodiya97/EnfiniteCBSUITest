0f9d40d4a3aed2c65e27421f39af10fd670004b7
mayurbharodiya97 <mayur.bharodiya@acuteinformatics.in>
2023-06-13 20:08:28 +0530

    added remaining tab content

--- src/pages_audit/pages/operations/c-kyc/PersonalDetails.tsx
+++ src/pages_audit/pages/operations/c-kyc/PersonalDetails.tsx
@@ -2,6 +2,279 @@ import React, { useEffect, useRef, useState } from 'react';
 import { Box, Grid, Typography, Paper, TextField, Button, Divider, Skeleton } from '@mui/material';
 import {styled} from "@mui/material/styles";
 import FormWrapper, {MetaDataType} from 'components/dyanmicForm';
+import {GridMetaDataType} from "../../../../components/dataTableStatic/types"
+import { GridWrapper } from 'components/dataTableStatic/gridWrapper'
+
+const ChequebookentryGridMetaData: GridMetaDataType = {
+  gridConfig: {
+    dense: true,
+    gridLabel: "Cheque Book Issued",
+    rowIdColumn: "id",
+    defaultColumnConfig: {
+      width: 100,
+      maxWidth: 200,
+      // minWidth: 100,
+    },
+    allowColumnReordering: true,
+    disableSorting: false,
+    disableGroupBy: true,
+    enablePagination: false,
+    hideFooter: true,
+    pageSizes: [10, 20, 30],
+    defaultPageSize: 10,
+    // containerHeight: {
+    //   min: "42vh",
+    //   max: "45vh",
+    // },
+    allowFilter: false,
+    allowColumnHiding: false,
+    allowRowSelection: false,
+  },
+  filters: [],
+  columns: [
+    {
+      accessor: "sr_no",
+      columnName: "Sr.No.",
+      sequence: 1,
+      alignment: "right",
+      componentType: "default",
+      width: 70,
+      minWidth: 70,
+      maxWidth: 200,
+      isAutoSequence: true,
+    },
+    {
+      accessor: "accountCardType",
+      columnName: "Type",
+      sequence: 2,
+      alignment: "left",
+      componentType: "default",
+      width: 100,
+      minWidth: 100,
+      maxWidth: 200,
+      isVisible: true,
+    },
+    {
+      accessor: "maskAcctCardNo",
+      columnName: "Account/Card Number",
+      sequence: 3,
+      alignment: "left",
+      componentType: "default",
+      width: 170,
+      minWidth: 150,
+      maxWidth: 200,
+    },
+    {
+      accessor: "customerId",
+      columnName: "Customer ID",
+      sequence: 4,
+      alignment: "left",
+      componentType: "default",
+      width: 120,
+      minWidth: 100,
+      maxWidth: 400,
+    },
+    {
+      accessor: "accountNameOrNameOnCard",
+      columnName: "Name",
+      sequence: 5,
+      alignment: "left",
+      componentType: "default",
+      width: 220,
+      minWidth: 180,
+      maxWidth: 250,
+    },
+    {
+      accessor: "constitution",
+      columnName: "Constitution",
+      sequence: 6,
+      alignment: "left",
+      componentType: "default",
+      width: 120,
+      minWidth: 100,
+      maxWidth: 200,
+    },
+    {
+      accessor: "productCode",
+      columnName: "Product Code",
+      sequence: 7,
+      alignment: "left",
+      componentType: "default",
+      width: 150,
+      minWidth: 100,
+      maxWidth: 200,
+    },
+    {
+      accessor: "productName",
+      columnName: "Account Type Name",
+      sequence: 8,
+      alignment: "left",
+      componentType: "default",
+      width: 120,
+      minWidth: 100,
+      maxWidth: 200,
+      // color: (value) => {
+      //   if ((value || "unlock").toLowerCase() === "unlock") {
+      //     return "green";
+      //   }
+      //   return "red";
+      // },
+    },
+    {
+      accessor: "cardProductBin",
+      columnName: "Card Product Bin",
+      sequence: 9,
+      alignment: "left",
+      componentType: "default",
+      width: 120,
+      minWidth: 100,
+      maxWidth: 200,
+    },
+    {
+      accessor: "dob",
+      columnName: "Birth Date",
+      sequence: 10,
+      alignment: "left",
+      componentType: "date",
+      dateFormat: "dd/MM/yyyy",
+      width: 120,
+      minWidth: 100,
+      maxWidth: 200,
+    },
+    {
+      accessor: "_NEW_gender",
+      columnName: "Gender",
+      sequence: 11,
+      alignment: "left",
+      componentType: "default",
+      width: 150,
+      minWidth: 100,
+      maxWidth: 400,
+    },
+    {
+      accessor: "fathersName",
+      columnName: "Father Name",
+      sequence: 12,
+      alignment: "left",
+      componentType: "default",
+      width: 150,
+      minWidth: 100,
+      maxWidth: 400,
+    },
+    {
+      accessor: "mothersName",
+      columnName: "Mother Name",
+      sequence: 13,
+      alignment: "left",
+      componentType: "default",
+      width: 150,
+      minWidth: 100,
+      maxWidth: 400,
+    },
+    {
+      accessor: "linkedAccount",
+      columnName: "Linked Account",
+      sequence: 14,
+      alignment: "left",
+      componentType: "default",
+      width: 150,
+      minWidth: 100,
+      maxWidth: 400,
+    },
+    {
+      accessor: "fullName",
+      columnName: "Full Name",
+      sequence: 15,
+      alignment: "left",
+      componentType: "default",
+      width: 150,
+      minWidth: 100,
+      maxWidth: 400,
+    },
+    {
+      accessor: "tinNumber",
+      columnName: "TIN Number",
+      sequence: 16,
+      alignment: "left",
+      componentType: "default",
+      width: 150,
+      minWidth: 100,
+      maxWidth: 400,
+    },
+    {
+      accessor: "nationalId13Digit",
+      columnName: "National ID 13 Digit",
+      sequence: 0,
+      alignment: "left",
+      componentType: "default",
+      width: 150,
+      minWidth: 100,
+      maxWidth: 400,
+      isVisible: false,
+    },
+    {
+      accessor: "modeOfOperation",
+      columnName: "Mode Of Operation",
+      sequence: 0,
+      alignment: "left",
+      componentType: "default",
+      width: 150,
+      minWidth: 100,
+      maxWidth: 400,
+      isVisible: false,
+    },
+    {
+      accessor: "nationality",
+      columnName: "Nationality",
+      sequence: 17,
+      alignment: "left",
+      componentType: "default",
+      width: 150,
+      minWidth: 100,
+      maxWidth: 400,
+    },
+    {
+      accessor: "nationalId",
+      columnName: "National ID",
+      sequence: 18,
+      alignment: "left",
+      componentType: "default",
+      width: 150,
+      minWidth: 100,
+      maxWidth: 400,
+    },
+    {
+      accessor: "identificationNo",
+      columnName: "Identification Number",
+      sequence: 19,
+      alignment: "left",
+      componentType: "default",
+      width: 150,
+      minWidth: 100,
+      maxWidth: 400,
+    },
+    {
+      accessor: "phone",
+      columnName: "Mobile Number",
+      sequence: 20,
+      alignment: "left",
+      componentType: "default",
+      width: 150,
+      minWidth: 100,
+      maxWidth: 400,
+    },
+    {
+      accessor: "email",
+      columnName: "E-Mail ID",
+      sequence: 21,
+      alignment: "left",
+      componentType: "default",
+      width: 150,
+      minWidth: 100,
+      maxWidth: 400,
+    },
+  ],
+}
 
 
 export const personal_detail_prefix_data = {
@@ -689,6 +962,40 @@ const myGridRef = useRef<any>(null);
                             formStyle={{}}
                             hideHeader={true}
                         />
+                    </Grid>                    
+                </Grid>
+            </Grid> : isLoading ? <Skeleton variant='rounded' animation="wave" height="300px" width="100%"></Skeleton> : null}
+
+            {isCustomerData ? 
+            <Grid 
+                sx={{
+                    backgroundColor:"var(--theme-color2)", 
+                    padding:(theme) => theme.spacing(2), 
+                    border: "1px solid rgba(0,0,0,0.12)", 
+                    borderRadius: "20px"
+                }} container item xs={12} direction={'column'}>
+                <Grid item>
+                    <Typography sx={{color:"var(--theme-color3)"}} gutterBottom={true} variant={"h6"}>Other Personal Details</Typography>
+                </Grid>
+                <Grid container item>
+                    <Grid item xs={12} sx={{backgroundColor:"#eee", padding: "100px", overflow: "hidden", boxSizing: "border-box"}}>
+                        {/* <FormWrapper 
+                            key={"new-form-in-kyc"}
+                            metaData={personal_other_detail_meta_data as MetaDataType}
+                            formStyle={{}}
+                            hideHeader={true}
+                        /> */}
+                      {/* <GridWrapper
+                        key={`ChequeBookEntryGrid`}
+                        finalMetaData={ChequebookentryGridMetaData as GridMetaDataType}
+                        // data={data?.[0]?.ALL_ACCOUNT_DETAIL ?? []}
+                        data={[]}
+                        setData={() => null}
+                        // loading={getData.isLoading}
+                        // setAction={setCurrentAction}
+                        refetchData={() => {}}
+                        ref={myGridRef}
+                      /> */}
                     </Grid>                    
                 </Grid>
             </Grid> : isLoading ? <Skeleton variant='rounded' animation="wave" height="300px" width="100%"></Skeleton> : null}
