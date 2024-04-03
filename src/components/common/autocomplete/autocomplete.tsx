bdc81a45c7a3f6ad188bffa2c1da29e2e5bf2a3d
Husen <husen.ghanchi@acuteinformatics.in>
2024-04-03 10:19:05 +0530

    Autocomplete paper width fixed -husen

--- src/components/common/autocomplete/autocomplete.tsx
+++ src/components/common/autocomplete/autocomplete.tsx
@@ -368,6 +368,7 @@ const MyAutocomplete: FC<MyAllAutocompleteProps> = ({
                   "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
                 overflow: "hidden",
                 maxWidth: "300px",
+                minWidth: "max(120px, 100%)"
               }}
             >
               {children}
