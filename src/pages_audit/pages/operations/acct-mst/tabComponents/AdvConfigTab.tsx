ef026dfcc1a7d2aa0e0c2593e0f64414ad93580f
mayurbharodiya97 <mayur.bharodiya@acuteinformatics.in>
2024-04-12 10:42:04 +0530

    added other add, document, adv configuration tabs

---
+++ src/pages_audit/pages/operations/acct-mst/tabComponents/AdvConfigTab.tsx
@@ -0,0 +1,68 @@
+import FormWrapper, { MetaDataType } from "components/dyanmicForm";
+import { useContext, useEffect, useMemo, useRef, useState } from "react";
+import { AcctMSTContext } from "../AcctMSTContext";
+import { Grid } from "@mui/material";
+import { advConnfig_tab_metadata } from "../tabMetadata/advConfigMetadata";
+import TabNavigate from "../TabNavigate";
+
+const AdvConfigTab = () => {
+  const { AcctMSTState, handleCurrFormctx, handleSavectx, handleStepStatusctx } = useContext(AcctMSTContext);
+  const formRef = useRef<any>(null);
+  const [isNextLoading, setIsNextLoading] = useState(false);
+  const [formStatus, setFormStatus] = useState<any[]>([])
+  const onSubmitPDHandler = () => {};
+  const initialVal:any= {}
+
+  const handleSave = (e) => {
+    handleCurrFormctx({
+      isLoading: true,
+    })
+    const refs = [formRef.current.handleSubmitError(e, "save", false)]
+    handleSavectx(e, refs)
+  }
+
+  useEffect(() => {
+    if(Boolean(AcctMSTState?.currentFormctx.currentFormRefctx && AcctMSTState?.currentFormctx.currentFormRefctx.length>0) && Boolean(formStatus && formStatus.length>0)) {
+      if(AcctMSTState?.currentFormctx.currentFormRefctx.length === formStatus.length) {
+        setIsNextLoading(false)
+        let submitted;
+        submitted = formStatus.filter(form => !Boolean(form))
+        if(submitted && Array.isArray(submitted) && submitted.length>0) {
+          submitted = false;
+        } else {
+          submitted = true;
+          handleStepStatusctx({
+            status: "completed",
+            coltabvalue: AcctMSTState?.colTabValuectx,
+          })
+        }
+        handleCurrFormctx({
+          currentFormSubmitted: submitted,
+          isLoading: false,
+        })
+        setFormStatus([])
+      }
+    }
+  }, [formStatus])
+
+  return (
+    <Grid sx={{ mb: 4 }}>
+      <FormWrapper
+        ref={formRef}
+        onSubmitHandler={onSubmitPDHandler}
+        // initialValues={AcctMSTState?.formDatactx["PERSONAL_DETAIL"] ?? {}}
+        initialValues={initialVal}
+        key={"pd-form-kyc" + initialVal}
+        metaData={advConnfig_tab_metadata as MetaDataType}
+        formStyle={{}}
+        formState={{GPARAM155: AcctMSTState?.gparam155 }}
+        hideHeader={true}
+        displayMode={AcctMSTState?.formmodectx}
+        controlsAtBottom={false}
+      ></FormWrapper>
+      <TabNavigate handleSave={handleSave} displayMode={AcctMSTState?.formmodectx ?? "new"} isNextLoading={isNextLoading} />
+    </Grid>
+  );
+};
+
+export default AdvConfigTab;
