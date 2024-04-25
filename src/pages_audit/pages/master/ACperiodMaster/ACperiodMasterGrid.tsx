b04f99fc2eb15522ccbb7226e4eba72a052014b5
Harshit Vadodariya <harshit.vadodariya@acuteinformatics.in>
2024-04-25 11:44:31 +0530

    A/c period master screen added

---
+++ src/pages_audit/pages/master/ACperiodMaster/ACperiodMasterGrid.tsx
@@ -0,0 +1,160 @@
+import { Fragment, useCallback, useContext, useEffect, useRef, useState } from "react";
+import { gridMetadata } from "./gridMetadata";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { ActionTypes } from "components/dataTable";
+import { GridMetaDataType } from "components/dataTableStatic";
+import GridWrapper  from "components/dataTableStatic/";
+import { PopupMessageAPIWrapper } from "components/custom/popupMessage";
+import { enqueueSnackbar } from "notistack";
+import { AcPeriodMasterFormWrapper } from "./viewDetails/ACperiodMasterViewDetails";
+import { useMutation, useQuery } from "react-query";
+import * as API from './api';
+import { AuthContext } from "pages_audit/auth";
+import { Alert } from "components/common/alert";
+import { queryClient } from "cache";
+
+
+const actions: ActionTypes[] = [
+  {
+ actionName: "add",
+ actionLabel: "Add",
+ multiple: undefined,
+ alwaysAvailable: true,
+},
+{
+ actionName: "view-details",
+ actionLabel: "View Detail",
+ multiple: false,
+ rowDoubleClick: true,
+},
+{
+ actionName: "Delete",
+ actionLabel: "Delete",
+ multiple: false,
+},
+];
+
+const LienMasterGrid = ()=> {
+  
+  const {authState} = useContext(AuthContext);
+  const [isDelete, SetDelete] = useState(false);
+  const navigate = useNavigate();
+  const isDeleteDataRef = useRef<any>(null);
+  const isDataChangedRef = useRef<any>(null);
+
+  const setCurrentAction = useCallback(
+    (data) => {
+      if (data?.name === "Delete") {
+        isDeleteDataRef.current = data?.rows?.[0];
+        SetDelete(true);
+      }
+      navigate(data?.name, {
+        state: data?.rows,
+      });
+    },
+    [navigate]
+);
+
+  const { data, isLoading, isFetching, isError, error, refetch } = useQuery<
+  any,
+  any
+>(["getInstallmentPeriodData"], () =>
+  API.getInstallmentPeriodData({
+    companyID: authState?.companyID,
+    branchCode: authState?.user?.branchCode,
+  })
+);
+const deleteMutation = useMutation(API.deleteInstallmentPeriodData, {
+  onError: (error: any) => {},
+  onSuccess: (data) => {
+    enqueueSnackbar("Records successfully deleted", {
+      variant: "success",
+    });
+    refetch();
+    SetDelete(false);
+  },
+});
+
+const onDeleteYes = (rows) => {
+  
+  deleteMutation.mutate({
+    ...rows?.data,
+    _isDeleteRow: true,
+  });
+};
+
+const ClosedEventCall = () => {
+  if (isDataChangedRef.current === true) {
+    isDataChangedRef.current = true;
+    refetch();
+    isDataChangedRef.current = false;
+  }
+  navigate(".");
+};
+  
+useEffect(() => {
+  return () => {
+    queryClient.removeQueries(["getInstallmentPeriodData"]);
+  };
+}, []);
+
+  return (
+    <Fragment>
+       {isError && (
+        <Alert
+          severity="error"
+          errorMsg={error?.error_msg ?? "Something went to wrong.."}
+          errorDetail={error?.error_detail}
+          color="error"
+        />
+      )}
+      <GridWrapper
+        key={"InstallmentPeriodGrid"}
+        finalMetaData={gridMetadata as GridMetaDataType}
+        data={data ?? []}
+        setData={() => null}
+        actions={actions}
+        loading={isLoading || isFetching}
+        setAction={setCurrentAction}
+        refetchData={() =>refetch()}
+        defaultSortOrder={[{ id: "LEAN_CD", desc: false }]}
+      />
+<Routes>
+      <Route
+          path="add/*"
+          element={
+            <AcPeriodMasterFormWrapper
+              isDataChangedRef={isDataChangedRef}
+              closeDialog={ClosedEventCall}
+              defaultView={"add"}
+            />
+          }
+        />
+        <Route
+          path="view-details/*"
+          element={
+            <AcPeriodMasterFormWrapper
+              isDataChangedRef={isDataChangedRef}
+              closeDialog={ClosedEventCall}
+              defaultView={"view"}
+            />
+          }
+        />
+      </Routes>
+       {isDelete ? (
+        <PopupMessageAPIWrapper
+          MessageTitle="Confirmation"
+          Message="Are you sure to delete selected row?"
+          onActionYes={(rowval) => onDeleteYes(rowval)}
+          onActionNo={() => SetDelete(false)}
+          rows={isDeleteDataRef.current}
+          open={isDelete}
+          loading={deleteMutation.isLoading}
+        />
+      ) : null}
+    </Fragment>
+  );
+};
+
+export default LienMasterGrid;
+
---
+++ src/pages_audit/pages/master/ACperiodMaster/viewDetails/metaData.tsx
@@ -0,0 +1,142 @@
+export const metaData = {
+  form: {
+    name: "AC Period Master",
+    label: "AC Period Master",
+    resetFieldOnUnmount: false,
+    validationRun: "onBlur",
+    submitAction: "home",
+    render: {
+      ordering: "auto",
+      renderType: "simple",
+      gridConfig: {
+        item: {
+          xs: 12,
+          sm: 12,
+          md: 12,
+        },  
+        container: {
+          direction: "row",
+          spacing: 1,
+        },
+      },
+    },
+    componentProps: {
+      textField: {
+        fullWidth: true,
+      },
+      select: {
+        fullWidth: true,
+      },
+      datePicker: {
+        fullWidth: true,
+      },
+      numberFormat: {
+        fullWidth: true,
+      },
+      inputMask: {
+        fullWidth: true,
+      },
+      datetimePicker: {
+        fullWidth: true,
+      },
+    },
+  },
+  fields: [
+    {
+      render: {
+        componentType: "textField",
+      },
+      name: "PERIOD_CD",
+      label: "Code",
+      placeholder: "Code",  
+      maxLength: 4,
+      type: "text",
+      required: true,
+      schemaValidation: {
+        type: "string",
+        rules: [{ name: "required", params: ["code is Required"] }],
+      },
+      GridProps: { xs: 12, sm: 12, md: 12, lg: 6, xl: 6 },
+      __EDIT__: {isReadOnly:true},
+    },
+    {
+      render: {
+        componentType: "textField",
+      },
+      name: "PERIOD_NM",
+      label: "Period Name",
+      placeholder: "Period Name",
+      maxLength: 40,
+      type: "text",
+      required: true,
+      schemaValidation: {
+        type: "string",
+        rules: [{ name: "required", params: ["Period Name is Required"] }],
+      },
+      GridProps: {  xs: 12, sm: 12, md: 12, lg: 6, xl:6 },
+     
+    },
+    {
+      render: {
+        componentType: "numberFormat",
+      },
+      className: "textInputFromLeft",
+      name: "INST_NO",
+      label: "Inst. No.",
+      placeholder: "Inst. No.",
+      FormatProps: {
+        allowNegative: false,
+        allowLeadingZeros: false,
+        decimalScale: 0,
+        isAllowed: (values) => {
+          if (values?.value?.length > 5) {
+            return false;
+          }
+          if (values.floatValue === 0) {
+            return false;
+          }
+          return true;
+        },
+      },
+      GridProps: {  xs: 12, sm: 12, md: 12, lg: 6, xl:6 },
+      schemaValidation: {
+        type: "string",
+        rules: [
+          { name: "required", params: ["Inst. No. is Required"] },
+          // { name: "max", params: [10, "Inst. No. should be 5 digit."] },
+        ],
+      },
+    },
+    {
+      render: { componentType: "select" },
+      name: "INSTALLMENT_TYPE",
+      placeholder: "Select Installment Period",
+      label: "Installment Period",
+      options: [
+        { label: "Daily ", value: "D"},
+        { label: "Monthly", value: "M"},
+        { label: "Quarterly", value: "Q"},
+        { label: "Half-Yearly", value: "H"},
+        { label: "Yearly", value: "Y"},
+        { label: "On Expiry", value: "E"},
+      ],
+      _optionsKey: "Installment_period",
+      defaultOptionLabel: "Select Installment Period",
+      defaultValue:"M",
+      required: true,
+      type: "text",
+      GridProps: {  xs: 12, sm: 12, md: 12, lg: 6, xl:6 },
+      fullWidth: true,
+      autoComplete: "on",
+      //@ts-ignore
+      isFieldFocused: false,
+      schemaValidation: {
+        type: "string",
+        rules: [
+          { name: "required", params: ["Please Select Installment Period"] },
+        ],
+      
+      },
+    },
+  ],
+};
