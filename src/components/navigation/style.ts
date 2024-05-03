cfc2c6540ba0da8de15dcb0ab4cd0c61b0df96fd
Husen <husen.ghanchi@acuteinformatics.in>
2024-05-03 10:28:04 +0530

    Canges in Side-bar height and aligment -husen

--- src/components/navigation/style.ts
+++ src/components/navigation/style.ts
@@ -105,7 +105,7 @@ export const useStylesSideBar = makeStyles((theme: any) => ({
     justifyContent: "left ",
   },
   navLinks: {
-    height: "calc(100vh - 78px)",
+    // height: "calc(100vh - 78px)",
     overflowY: "auto",
     overflowX: "hidden",
     // height:"100px",
@@ -121,7 +121,7 @@ export const useStylesSideBar = makeStyles((theme: any) => ({
   navLinksforseparateView: {
     overflowY: "auto",
     overflowX: "hidden",
-    height: "calc(100vh - 160px)",
+    height: "calc(100vh - 180px)",
     // height:"auto",
     paddingTop: "10px",
   },
