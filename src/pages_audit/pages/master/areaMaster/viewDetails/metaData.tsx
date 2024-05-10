6ead97610f69df8be89b1c6ab0fd477baec3ced3
kinjal sutarsandhiya <kinjal.sutarsandhiya@acuteinformatics.in>
2024-05-10 17:26:14 +0530

    Area master screen added

---
+++ src/pages_audit/pages/master/areaMaster/viewDetails/metaData.tsx
@@ -0,0 +1,100 @@
+
+import * as API from '../api';
+export const AreaMasterMetaData = {
+  form: {
+    name: "Area Master",
+    label: "Area Master",
+    resetFieldOnUnmount: false,
+    validationRun: "onBlur",
+    submitAction: "home",
+    render: {
+      ordering: "auto",
+      renderType: "simple",
+      gridConfig: {
+        item: {
+          xs: 12,
+          sm: 12,
+          md: 12,
+          lg:12,
+          xl:12
+        },
+        container: {
+          direction: "row",
+          spacing: 2,
+          width:200,
+          maxwidth:500
+        },
+      },
+    },
+    componentProps: {
+      textField: {
+        fullWidth: true,
+      },
+      autoComplete: {
+        fullWidth: true,
+      },
+      numberFormat: {
+        fullWidth: true,
+      },
+    },
+  },
+  fields: [
+    {
+      render: {
+        componentType: "textField",
+      },
+      name: "AREA_CD",
+      label: "Code",
+      placeholder: "Code",
+      type: "text",
+      required: true,
+      schemaValidation: {
+        type: "string",
+        rules: [{ name: "required", params: ["code is Required"] }],
+      },
+      GridProps: { xs: 12, sm: 3, md: 3, lg: 3, xl:3  },
+      __EDIT__:{isReadOnly:true}
+    },
+    {
+      render: { componentType: "textField" },
+      name: "AREA_NM",
+      label: "Name",
+      type: "text",
+      required: true, 
+      placeholder: "Name",
+      maxLength:100,
+      schemaValidation: {
+        type: "string",
+        rules: [{ name: "required", params: ["Description is Required"] }],
+      },
+      GridProps: {  xs: 12, sm: 6, md: 6, lg: 6, xl:6 },
+    },
+    {
+      render: { componentType: "numberFormat" },
+      name: "PIN_CODE",
+      label: "Pin Code",
+      placeholder: "Pin Code",
+      require:false,
+      GridProps: {  xs: 12, sm: 3, md: 3, lg: 3, xl:3 },
+    },
+    {
+      render: { componentType: "autocomplete" },
+      name: "CITY_CD",
+      label: "City", 
+      options:API.GETAREAMSTCITYDDW,
+      _optionsKey: "getAreaMstCityddw",
+      __VIEW__: { isReadOnly: true },
+      GridProps: {  xs: 12, sm: 6, md: 6, lg: 6, xl:6},
+    },
+    {
+      render: { componentType: "autocomplete" },
+      name: "PARENT_AREA",
+      label: "Parent Area",
+      options: API.GETAREAMSTPARENTDDW,
+      __VIEW__: { isReadOnly: true },
+      _optionsKey: "getAreaMstParentddw",
+      GridProps: {  xs: 12, sm: 6, md: 6, lg: 6, xl: 6 }
+    },
+  ],
+};
+
--- src/pages_audit/pages/master/master.tsx
+++ src/pages_audit/pages/master/master.tsx
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router-dom";
 const Prioritymain = lazy(() => import('./priorityMasterMain/priorityMasterMainGrid'));
 const Prioritymastersub = lazy(()=> import('./priorityMasterSub/priorityMasterSubGrid'))
 const TradeMaster = lazy(()=> import('./tradeMaster/tradeMasterGrid'))
+const AreaMaster = lazy(()=> import('./areaMaster/areaMasterGrid'))
 
 const Master = () => {
   return (
@@ -13,6 +14,7 @@ const Master = () => {
       <Route path="priority-master-main/*" element={<Prioritymain />} />
       <Route path="priority-master-sub/*" element={<Prioritymastersub />} />
       <Route path="trade-master/*" element={<TradeMaster />} />
+      <Route path="area-master/*" element={<AreaMaster />} />
     </Routes>
     </Fragment>
   );
