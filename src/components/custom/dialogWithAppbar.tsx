9e16d595ae7d779273902353c1a774410d5deaca
NarendraDabhi6113 <narendra.dabhi@acuteinformatics.in>
2024-06-07 17:24:48 +0530

    dialog with appbar changes

--- src/components/custom/dialogWithAppbar.tsx
+++ src/components/custom/dialogWithAppbar.tsx
@@ -41,24 +41,28 @@ const DialogWithAppbar = ({
           isNewStyle={true}
         />
       </Box>
-      <DialogTitle
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          background: "var(--theme-color5)",
-          margin: "10px 32px 0px 32px",
-          alignItems: "center",
-          height: "7vh",
-          boxShadow:
-            "0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)",
-        }}
-      >
-        {title}
-      </DialogTitle>
-      <DialogContent sx={{ paddingTop: "10px", paddingBottom: "0px" }}>
-        {content}
-      </DialogContent>
-      <DialogActions>{actions}</DialogActions>
+      {Boolean(title) && (
+        <DialogTitle
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            background: "var(--theme-color5)",
+            margin: "10px 32px 0px 32px",
+            alignItems: "center",
+            height: "7vh",
+            boxShadow:
+              "0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)",
+          }}
+        >
+          {title}
+        </DialogTitle>
+      )}
+      {Boolean(content) && (
+        <DialogContent sx={{ paddingTop: "10px", paddingBottom: "0px" }}>
+          {content}
+        </DialogContent>
+      )}
+      {Boolean(actions) && <DialogActions>{actions}</DialogActions>}{" "}
     </Dialog>
   );
 };
