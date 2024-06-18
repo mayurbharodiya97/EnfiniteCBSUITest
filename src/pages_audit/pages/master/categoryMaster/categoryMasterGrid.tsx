fecae88fb53bfb2406f38de46ab03c9441602032
rushi-butani <rushi.butani@acuteinformatics.in>
2024-06-18 11:28:45 +0530

    Add messagebox, duplication validation, acctno api code change and dml code change in category mst screen

--- src/pages_audit/pages/master/categoryMaster/categoryMasterGrid.tsx
+++ src/pages_audit/pages/master/categoryMaster/categoryMasterGrid.tsx
@@ -1,23 +1,16 @@
-import {
-  Fragment,
-  useRef,
-  useCallback,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
+import { useRef, useCallback, useContext, useEffect } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import GridWrapper from "components/dataTableStatic";
 import { GridMetaDataType, ActionTypes } from "components/dataTable/types";
 import { CategoryMasterGridMetaData } from "./gridMetadata";
 import { Alert } from "components/common/alert";
-import { CategoryMasterFormWrapper } from "./categoryMasterForm/categoryMasterForm";
 import { useMutation, useQuery } from "react-query";
 import * as API from "./api";
 import { AuthContext } from "pages_audit/auth";
 import { ClearCacheContext, queryClient } from "cache";
-import { PopupMessageAPIWrapper } from "components/custom/popupMessage";
 import { enqueueSnackbar } from "notistack";
+import { CategoryMasterFormWrapper } from "./categoryMasterForm";
+import { usePopupContext } from "components/custom/popupContext";
 
 const actions: ActionTypes[] = [
   {
@@ -44,16 +37,47 @@ const actions: ActionTypes[] = [
 export const CategoryMasterGrid = () => {
   const navigate = useNavigate();
   const isDataChangedRef = useRef(false);
-  const [isDelete, setDelete] = useState(false);
   const { getEntries } = useContext(ClearCacheContext);
   const isDeleteDataRef = useRef<any>(null);
   const { authState } = useContext(AuthContext);
+  const { MessageBox, CloseMessageBox } = usePopupContext();
+
+  const deleteMutation = useMutation(API.categoryMasterDML, {
+    onError: (error: any) => {
+      let errorMsg = "Unknown Error occured";
+      if (typeof error === "object") {
+        errorMsg = error?.error_msg ?? errorMsg;
+      }
+      enqueueSnackbar(errorMsg, {
+        variant: "error",
+      });
+      CloseMessageBox();
+    },
+    onSuccess: () => {
+      enqueueSnackbar("Record successfully deleted", {
+        variant: "success",
+      });
+      CloseMessageBox();
+      refetch();
+    },
+  });
 
   const setCurrentAction = useCallback(
-    (data) => {
+    async (data) => {
       if (data?.name === "delete") {
         isDeleteDataRef.current = data?.rows?.[0];
-        setDelete(true);
+        const btnName = await MessageBox({
+          message: "Are you sure to delete selected row?",
+          messageTitle: "Confirmation",
+          buttonNames: ["Yes", "No"],
+          loadingBtnName: "Yes",
+        });
+        if (btnName === "Yes") {
+          deleteMutation.mutate({
+            ...isDeleteDataRef.current?.data,
+            _isDeleteRow: true,
+          });
+        }
       } else if (data?.name === "add") {
         navigate(data?.name, {
           state: [],
@@ -100,26 +124,8 @@ export const CategoryMasterGrid = () => {
     }
   }, [navigate]);
 
-  const deleteMutation = useMutation(API.deleteCategoryMasterData, {
-    onError: (error: any) => {},
-    onSuccess: () => {
-      enqueueSnackbar("Record successfully deleted", {
-        variant: "success",
-      });
-      refetch();
-      setDelete(false);
-    },
-  });
-
-  const onDeleteYes = (rows) => {
-    deleteMutation.mutate({
-      ...rows?.data,
-      _isDeleteRow: true,
-    });
-  };
-
   return (
-    <Fragment>
+    <>
       {isError && (
         <Alert
           severity="error"
@@ -146,6 +152,7 @@ export const CategoryMasterGrid = () => {
               isDataChangedRef={isDataChangedRef}
               closeDialog={handleDialogClose}
               defaultView={"new"}
+              gridData={data}
             />
           }
         />
@@ -156,22 +163,11 @@ export const CategoryMasterGrid = () => {
               isDataChangedRef={isDataChangedRef}
               closeDialog={handleDialogClose}
               defaultView={"view"}
+              gridData={data}
             />
           }
         />
       </Routes>
-
-      {isDelete ? (
-        <PopupMessageAPIWrapper
-          MessageTitle="Confirmation"
-          Message="Are you sure to delete selected row?"
-          onActionYes={(rows) => onDeleteYes(rows)}
-          onActionNo={() => setDelete(false)}
-          rows={isDeleteDataRef.current}
-          open={isDelete}
-          loading={deleteMutation.isLoading}
-        />
-      ) : null}
-    </Fragment>
+    </>
   );
 };
