493bf36c6566571f05b5889590516f88d84c2e64
mishva patel <mishva.patel@acuteinformatics.in>
2024-08-03 15:35:18 +0530

    Change in language in form 15GH screen

--- src/components/multiLanguage/en-language.ts
+++ src/components/multiLanguage/en-language.ts
@@ -1425,6 +1425,23 @@ export const en_language_data = {
   billtypeRequired: "Bill Type is Required",
   payslipNoRequired: "Payslip Number is Required",
 
+  // Positive pay
+  AccountNum: "Account Number",
+  ChequeNumber: "Cheque Number",
+  RequestFrom: "Request From",
+  TransactionDate: "Transaction Date",
+  UploadImage: "Upload Image",
+  ResponseParameters: "Response Parameters",
+  SelectCofiguration: "Select Cofiguration",
+  EnterPayeeName: "Enter Payee Name",
+  ChequeNumberIsRequired: "Cheque Number is Required.",
+  ChequeDateIsRequired: "Cheque Date is Required.",
+  AmountIsRequired: "Amount is Required.",
+  ChequeImage: "Cheque Image",
+  ChequeImageIsNotUploaded: "Cheque Image is not uploaded.",
+  RemovalRemarksPositivePay: "Removal Remarks For Positive Pay Confirmation",
+  Unknownerroroccured: "Unknown error occured",
+
   // Form 15GH entry and confirmation
   NoteDoubleClickOnViewDetails: "Note : Double-click to View-Details.",
   FormName: "Form Name",
@@ -1463,4 +1480,6 @@ export const en_language_data = {
   DeleteWarning: "Delete Warning",
   CannotDeleteConfirmedForm: "Cannot Delete Confirmed Form.",
   PrintFormConfirmation: "Do you want to print form?",
+  Success: "Success",
+  DataUpdatedSuccessfully: "Data Updated Successfully",
 };
