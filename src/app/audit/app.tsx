b101e2cff55c11185dfcd863d11e86b3e8e3c441
Harshit Vadodariya <harshit.vadodariya@acuteinformatics.in>
2024-08-03 14:22:24 +0530

    app.tsx updated with npm acute common base package

--- src/app/audit/app.tsx
+++ src/app/audit/app.tsx
@@ -1,10 +1,13 @@
+import {
+  WorkerContextProvider,
+  PopupContextProvider,
+  SnackbarProviderWrapper,
+  queryClient,
+} from "@acuteinfo/common-base";
+
 import { RecoilRoot } from "recoil";
 import { QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
-import { SnackbarProvider } from "notistack";
-import { queryClient } from "cache";
 import "registry/fns/registerFnsCbsEnfinity";
-// import "components/tableCellComponents";
 import IndexPage from "pages_audit";
 import { theme } from "./theme";
 import "./index.css";
@@ -14,18 +17,10 @@ import {
   unstable_createMuiStrictModeTheme,
 } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import { WorkerContextProvider } from "pages_audit/pages/reports/context/exportWorkerContext";
 import { CustomSnackbarContent } from "components/customNotification/customNotistack";
-import { PopupContextProvider } from "components/custom/popupContext";
 import { ThemeProviders } from "./ThemeProvider";
 
 const themeObj = unstable_createMuiStrictModeTheme(theme);
-
-declare module "notistack" {
-  interface VariantOverrides {
-    customSnackbar: true;
-  }
-}
 
 export const App = () => {
   return (
@@ -37,13 +32,13 @@ export const App = () => {
             <PopupContextProvider>
               <WorkerContextProvider>
                 <ThemeProviders>
-                  <SnackbarProvider
+                  <SnackbarProviderWrapper
                     maxSnack={3}
                     autoHideDuration={5000}
-                    Components={{ customSnackbar: CustomSnackbarContent }}
+                    Components={{ exportReportSnackbar: CustomSnackbarContent }}
                   >
                     <IndexPage />
-                  </SnackbarProvider>
+                  </SnackbarProviderWrapper>
                 </ThemeProviders>
               </WorkerContextProvider>
               {/* {process.env.NODE_ENV !== "production" ? (
