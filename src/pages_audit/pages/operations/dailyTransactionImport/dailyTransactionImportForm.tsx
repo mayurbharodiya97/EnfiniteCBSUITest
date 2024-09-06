e6a51fed26915f1e84ab4c69130b9ca21448b76f
Bhavyata Savaliya <bhavyata.savaliya@acuteinformatics.in>
2024-09-06 17:58:03 +0530

    create ui for file upload in daily import form

--- src/pages_audit/pages/operations/dailyTransactionImport/dailyTransactionImportForm.tsx
+++ src/pages_audit/pages/operations/dailyTransactionImport/dailyTransactionImportForm.tsx
@@ -10,7 +10,10 @@ import FormWrapper, { MetaDataType } from "components/dyanmicForm";
 import { usePopupContext } from "components/custom/popupContext";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import { GridMetaDataType } from "components/dataTableStatic";
-import { DailyTransactionImportGridMetaData, DailyTransactionImportMetadata } from "./dailyTransactionImportMetadata";
+import {
+  DailyTransactionImportGridMetaData,
+  DailyTransactionImportMetadata,
+} from "./dailyTransactionImportMetadata";
 import { ActionTypes } from "components/dataTable";
 import { Alert } from "components/common/alert";
 import { AuthContext } from "pages_audit/auth";
@@ -22,13 +25,15 @@ import { useMutation } from "react-query";
 import { enqueueSnackbar } from "notistack";
 import { SubmitFnType } from "packages/form";
 import { GradientButton } from "components/styledComponent/button";
+import { Dialog } from "@mui/material";
+import { FileUploadControl } from "components/fileUpload";
 
 const actions: ActionTypes[] = [
   {
     actionName: "errors",
     actionLabel: t("Errors"),
     multiple: false,
-    alwaysAvailable: true
+    alwaysAvailable: true,
     // rowDoubleClick: true,
   },
 ];
@@ -38,8 +43,9 @@ const DailyTransactionImport = () => {
   const { MessageBox, CloseMessageBox } = usePopupContext();
   const { t } = useTranslation();
   const navigate = useNavigate();
-  const [gridData, setGridData] = useState<any>([])
+  const [gridData, setGridData] = useState<any>([]);
   const hasUpdated = useRef(false);
+  const [isSelectFileOpen, setIsSelectFileOpen] = useState(false);
 
   const setCurrentAction = useCallback(
     async (data) => {
@@ -64,8 +70,7 @@ const DailyTransactionImport = () => {
           variant: "error",
         });
       },
-      onSuccess: async (data, variables) => {
-      },
+      onSuccess: async (data, variables) => {},
     }
   );
   const getValidateToSelectFile: any = useMutation(
@@ -83,15 +88,7 @@ const DailyTransactionImport = () => {
       onSuccess: async (data, variables) => {
         for (let i = 0; i < data?.length; i++) {
           if (data[i]?.O_STATUS === "0") {
-            const buttonName = await MessageBox({
-              messageTitle: t("ValidationSuccessful"),
-              message: t("AreYouSurePostThisCheque"),
-              buttonNames: ["No", "Yes"],
-              loadingBtnName: ["Yes"],
-            });
-            // if (buttonName === "Yes") {
-
-            // }
+            setIsSelectFileOpen(true);
           } else if (data[i]?.O_STATUS === "9") {
             MessageBox({
               messageTitle: t("Alert"),
@@ -141,9 +138,8 @@ const DailyTransactionImport = () => {
         WORKING_DATE: authState.workingDate,
         USERNAME: authState?.user?.name,
         USERROLE: authState?.role,
-        ...data
-
-      })
+        ...data,
+      });
     } else {
       getDailyTransactionUploadData.mutate({
         ...data,
@@ -151,22 +147,26 @@ const DailyTransactionImport = () => {
         BRANCH_CD: authState?.user?.branchCode,
         ACCT_TYPE: data?.FROM_ACCT_TYPE,
         ACCT_CD: data?.FROM_ACCT_CD,
-        FLAG: "P"
-      })
-
+        FLAG: "P",
+      });
     }
     endSubmit(true);
   };
 
-  if (DailyTransactionImportGridMetaData && gridData.length > 0 && !hasUpdated.current) {
+  if (
+    DailyTransactionImportGridMetaData &&
+    gridData.length > 0 &&
+    !hasUpdated.current
+  ) {
     DailyTransactionImportGridMetaData.gridConfig.gridLabel =
-      DailyTransactionImportGridMetaData.gridConfig.gridLabel + " " + gridData[0]?.DEBIT_AC;
+      DailyTransactionImportGridMetaData.gridConfig.gridLabel +
+      " " +
+      gridData[0]?.DEBIT_AC;
     hasUpdated.current = true; // Set flag to true to prevent further updates
   }
 
   return (
     <>
-
       <FormWrapper
         key={"DailyTransactionImportForm"}
         metaData={DailyTransactionImportMetadata}
@@ -180,13 +180,12 @@ const DailyTransactionImport = () => {
         }}
         ref={formRef}
         onFormButtonClickHandel={() => {
-          let event: any = { preventDefault: () => { } };
+          let event: any = { preventDefault: () => {} };
           formRef?.current?.handleSubmit(event, "SELECT");
         }}
         setDataOnFieldChange={(action, payload) => {
           if (action === "GRID_DETAIL") {
             setGridData(payload);
-
           }
         }}
       >
@@ -204,7 +203,7 @@ const DailyTransactionImport = () => {
             </GradientButton>
           </>
         )}
-      </FormWrapper >
+      </FormWrapper>
 
       <>
         <GridWrapper
@@ -215,11 +214,34 @@ const DailyTransactionImport = () => {
           // loading={getInsuranceDetailData.isLoading}
           actions={actions}
           setAction={setCurrentAction}
-
         />
       </>
 
-
+      {isSelectFileOpen && (
+        <>
+          <Dialog fullWidth maxWidth="md" open={isSelectFileOpen}>
+            <FileUploadControl
+              key={"DailyTransactionFileUploadData"}
+              onClose={() => {
+                setIsSelectFileOpen(false);
+              }}
+              editableFileName={false}
+              defaultFileData={[]}
+              onUpload={async (
+                formDataObj,
+                proccessFunc,
+                ResultFunc,
+                base64Object,
+                result
+              ) => {}}
+              gridProps={{}}
+              maxAllowedSize={1024 * 1204 * 10} //10Mb file
+              allowedExtensions={["xlsx", "pdf", "csv", "txt", "xls", "TXT"]}
+              onUpdateFileData={(files) => {}}
+            />
+          </Dialog>
+        </>
+      )}
     </>
   );
 };
