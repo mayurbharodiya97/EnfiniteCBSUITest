4b93ffdffc804e1875150cf37cae59f0a7c27a5f
Harshit Vadodariya <harshit.vadodariya@acuteinformatics.in>
2024-09-24 19:58:47 +0530

    files deleted

--- src/pages_audit/common/transition.tsx
+++
@@ -1,6 +0,0 @@
-import { forwardRef } from "react";
-import Slide from "@mui/material/Slide";
-export const Transition = forwardRef(function Transition(props, ref) {
-  //@ts-ignore
-  return <Slide direction="up" ref={ref} {...props} />;
-});
