7bb9e82b88346118b5ec7052bb5eb56c8c1709a6
Bhavyata Savaliya <bhavyata.savaliya@acuteinformatics.in>
2024-10-09 18:32:01 +0530

    changes in cts outword

--- src/pages_audit/pages/operations/ctsOutward/confirmation/CtsOutwardClearingForm.tsx
+++ src/pages_audit/pages/operations/ctsOutward/confirmation/CtsOutwardClearingForm.tsx
@@ -323,7 +323,7 @@ const CtsOutwardAndInwardReturnConfirm: FC<{
                     }
                   }}
                 >
-                  {t("Remove")}
+                  {t("Reject")}
                 </GradientButton>
                 <GradientButton
                   onClick={() => {
