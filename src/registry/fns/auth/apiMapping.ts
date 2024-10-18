1cf1f78e5f791c0b08ee359cb6b696c1ba73d685
Husen <husen.ghanchi@acuteinformatics.in>
2024-10-18 15:20:19 +0530

    Payslip Printing code updated -Husen

--- src/registry/fns/auth/apiMapping.ts
+++ src/registry/fns/auth/apiMapping.ts
@@ -2885,6 +2885,10 @@ export const ActionWiseAPIConfiguration = {
     url: "/enfinityReportServiceAPI/GETFDDETAILREPORT",
     packageName: "",
   },
+  GETDDPRINTCONFIGDTL: {
+    url: "/enfinityCommonServiceAPI/GETDYNAMICDATA/GETDDPRINTCONFIGDTL",
+    packageName: "",
+  },
 };
 
 export const GetAPIURLFromAction = (action, pname) => {
